5a2c7a80339e36e436d4c338a5acf5f9f59a9ef0
Tasnim Zotder <tasnimzotder@gmail.com>
2024-03-03 01:12:34 +0530

    add traefik proxy

--- server/api/server.api.go
+++ server/api/server.api.go
@@ -3,9 +3,10 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/tasnimzotder/tchat/server/models"
 	"log"
 	"net/http"
+
+	"github.com/tasnimzotder/tchat/server/models"
 )
 
 type ServerAPI struct {
@@ -24,6 +25,7 @@ func (s *ServerAPI) Start(port string) {
 	log.Println("Starting server")
 
 	// routes
+	http.HandleFunc("/health", s.healthCheckHandler)
 	http.HandleFunc("/ping", s.PingHandler)
 	http.HandleFunc("/v1/user/create", s.createUserHandler)
 	http.HandleFunc("/v1/message/send", s.sendMessageHandler)
@@ -47,4 +49,12 @@ func (s *ServerAPI) PingHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(resp)
+}
+
+func (s *ServerAPI) healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
+	log.Printf("Health check request received")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(`{"status": "ok"}`))
 }
--- webhook/webhook_listener.py
+++ webhook/webhook_listener.py
@@ -10,6 +10,10 @@ WORK_DIR = '/app'
 GIT_REPO = 'https://github.com/tasnimzotder/tchat.git'
 
 
+def read_and_strip(command):
+    return command.read().split('\n')[0].strip()
+
+
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
     if request.json and request.json.get('ref') == 'refs/heads/main':
@@ -23,12 +27,72 @@ def handle_webhook():
         os.system("cd {}/tchat".format(WORK_DIR))
         os.system("git pull origin main")
 
-        # build and run the docker containers
-        os.system("docker compose -f {}/tchat/server/compose.yaml up -d --build".format(WORK_DIR))
+        # create a file acme.json for traefik if not exists
+        if not os.path.exists('/app/tchat/acme.json'):
+            with open('/app/tchat/acme.json', 'w') as f:
+                f.write('{}')
 
-        return 'Deployment triggered', 200
+        # build and run the docker containers
+        os.system(
+            "docker compose -f {}/tchat/compose.yaml up -d --build".format(WORK_DIR))
+
+        res = {
+            "status": "OK",
+            "message": "Deployment triggered"
+        }
+
+        return res, 200
+
     else:
-        return 'Invalid webhook', 400
+        res = {
+            "status": "ERROR",
+            "message": "Invalid webhook"
+        }
+
+        return res, 400
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    os_name = os.popen('uname').read().split('\n')[0]
+
+    system_info = {}
+    container_info = {}
+
+    if os_name == 'Darwin':
+        # get system information
+        system_info["uptime"] = read_and_strip(os.popen('uptime'))
+        system_info["memory_usage"] = read_and_strip(
+            os.popen('top -l 1 | grep PhysMem'))
+        system_info["cpu_usage"] = read_and_strip(
+            os.popen('top -l 1 | grep CPU'))
+
+    elif os_name == 'Linux':
+        system_info["uptime"] = read_and_strip(os.popen('uptime -p'))
+        system_info["memory_usage"] = read_and_strip(os.popen('free -m'))
+        system_info["cpu_usage"] = read_and_strip(
+            os.popen('top -bn1 | grep load'))
+
+    # get container information
+    container_info["container_count"] = read_and_strip(
+        os.popen('docker ps -aq | wc -l'))
+    container_info["running_container_count"] = read_and_strip(
+        os.popen('docker ps -q | wc -l'))
+    container_info["image_count"] = read_and_strip(
+        os.popen('docker images -q | wc -l'))
+    container_info["volume_count"] = read_and_strip(
+        os.popen('docker volume ls -q | wc -l'))
+    container_info["network_count"] = read_and_strip(
+        os.popen('docker network ls -q | wc -l'))
+
+    res = {
+        "status": "OK",
+        "version": "0.0.1",
+        "system_info": system_info,
+        "container_info": container_info,
+    }
+
+    return res, 200
 
 
 if __name__ == '__main__':
